19bb3969176b902ade579e1571872a927ceb3ec9
M-Tonin <marcosvtonin@hotmail.com>
2017-06-09 23:31:51 -0300

    ATUALIZANDO SHARED E EXECUTE

--- src/execute.h
+++ src/execute.h
@@ -22,13 +22,208 @@ SC_MODULE(execute){
 			recebimento = decode_execute.read();
 			escrita = recebimento;
 
-			e_rs = recebimento->rs;
-			e_rt = recebimento-> rt;
+			e_rs = recebimento->rs2;
+			e_rt = recebimento-> rs1;
 			e_rd = recebimento->rd;
-			e_op = recebimento-> op;
-			e_k8 = recebimento->k8;
-			e_k4 = recebimento->k4;
+			e_op = recebimento-> opcode;
+			e_funct7 = recebimento->funct7;
+			e_funct3 = recebimento->funct3;
+			e_imm_I = recebimento->Imm_I;
+			e_imm_U = recebimento->Imm_U;
+			e_imm_J = recebimento->Imm_J;
+			e_imm_B = recebimento->Imm_B;
+			e_shamt = recebimento->shamt;
+			// switch
+			switch(e_op){
 
+				case  u_LUI: 
+						// operacao lui
+				break;	
+				case  u_AUIPC: 
+						// operacao AUIPC
+				break;
+				case  TIPO_JAL: 
+						// operacao JAL
+				break;
+				case  TIPO_I_JALR: 
+						// operacao JAL
+						if(e_funct3 == 0x0){
+							// operacao JAL				
+						}
+				break;
+				case  TIPO_B: 
+				// branches -> tipo B
+					switch(e_funct3){
+						case f3_BEQ:
+						// funcao beq
+						break;
+						case f3_BNE:
+						// funcao bne
+						break;
+						case f3_BLT:
+						// funcao bLT
+						break;
+						case f3_BGE:
+						// funcao bGe
+						break;
+						case f3_BLT:
+						// funcao bLTU
+						break;
+						case f3_BGE:
+						// funcao bGeU
+						break; // default?
+									}// fim SWITCHh funct3 - Branch		
+				break;
+				case TIPO_I_RESTO :	
+				// operacoes loads
+					switch(e_funct3){
+						case f3_LB: 
+						// load byte
+						break;
+						case f3_LH: 
+						// load half
+						break;
+						case f3_LW: 
+						// load word
+						break;	
+						case f3_LBU: 
+						// load byte unsigned
+						break;
+						case f3_LHU: 
+						// load half unsigned
+						break;
+							// default?
+					} // fim switch funct3 - loads
+				break;
+				case TIPO_S:
+				// funcoes do tipo store
+					switch(e_funct3){
+						case f3_SB: 
+						// store byte
+						break;
+						case f3_SH: 
+						// store half
+						break;
+						case f3_SW: 
+						// store word
+						break;	
+						// default?	
+					} // fim switch funct3 - STORE
+				break;
+				case TIPO_I2_SHAMT:
+				// tipos i e shifts
+					switch(e_funct3){
+						case f3_ADDI:
+						// addi 	
+						break;
+						case f3_SLTI:
+						// slti
+						break;
+						case f3_SLTIU:
+						// sltiu
+						break;
+						case f3_XORI:
+						// XORI
+						break;
+						case f3_ORI:
+						// ORI
+						break;
+						case f3_ANDI:
+						// ANDI
+						break;
+						case f3_SRLI_SRAI:
+						// srli ou srai
+							if(e_funct7 == f7_SRAI){
+								// srai
+							}
+							if(e_funct7 == f7_RESTO_I){
+								// SRLI
+							}
+						break;
+						case f3_SLLI:
+						// SLLI
+							if(e_funct7 == f7_RESTO_I ){
+								// SLLI
+							}
+						break;
+				case TIPO_R:
+
+					switch(e_funct7){
+
+							case f7_RESTO:
+								switch(e_funct3){
+									case f3_ADD_SUB:
+									// add
+									break;	
+									case f3_SLL:
+									// Sll
+									break;	
+									case f3_SLT:
+									// SlT
+									break;
+									case f3_SLTU:
+									// SlTU
+									break;
+									case f3_XOR:
+									// xor
+									break;
+									case f3_SRL_SRA:
+									// SRL
+									break;
+									case f3_OR:
+									// or
+									break;
+									case f3_AND:
+									// or
+									break;
+									
+								} // fim funct 3 -> f7 resto
+							break;
+							case f7_SRA_SUB:
+								switch(e_funct3){
+         							case f3_ADD_SUB:
+									// sub
+									break;
+									case f3_SRL_SRA:
+									// SRA
+									break;
+								}// fim funct 3 -> f7 SRA_SUB
+
+							break;
+
+					}// fim switch funct 7
+
+
+				break;		
+					
+					}// fim switch funct3 - imediato
+
+
+
+
+								}// FIM SWITCH 		
+				p_breg->write(0, 0);
+				escrita->pc = recebimento-> pc;
+				execute_fetch.write(escrita);
+			} //while
+
+		} // execute_method()
+
+	SC_CTOR(execute){
+		recebimento = new contexto();
+		escrita = new contexto();
+		SC_THREAD(execute_method);
+	}
+
+private:
+		contexto *recebimento, *escrita;
+		unsigned short e_rs1, e_rs2, e_rd, e_op,e_funct3,e_funct7,e_shamt;
+		short e_imm_I,e_imm_U,e_imm_S,e_imm_B,e_imm_J;
+
+
+};
+
+/*	
 			switch (e_op) {
 					case i_ADD:   p_breg->write(e_rd, (p_breg->read(e_rs)+ p_breg->read(e_rt)));
 								  break;
@@ -70,23 +265,4 @@ SC_MODULE(execute){
 								 recebimento->pc = p_breg->read(e_rs) + e_k8;
 								 break;
 				} // switch
-				p_breg->write(0, 0);
-				escrita->pc = recebimento-> pc;
-				execute_fetch.write(escrita);
-			} //while
-
-		} // execute_method()
-
-	SC_CTOR(execute){
-		recebimento = new contexto();
-		escrita = new contexto();
-		SC_THREAD(execute_method);
-	}
-
-private:
-		contexto *recebimento, *escrita;
-		unsigned short e_rs, e_rt, e_rd, e_op;
-		short e_k4, e_k8;
-};
-
-
+*/
--- src/shared.h
+++ src/shared.h
@@ -36,29 +36,77 @@ typedef struct{
 
 } contexto;
 
+
+// OPCODES QUE NÃO SE ALTERAM ....
 enum i_FORMAT {
-	TIPO_R=4, TIPO_J=3
+	TIPO_R=0x33,
+	TIPO_S= 0x23,
+	TIPO_I2_SHAMT=0x13,	
+	TIPO_B = 0x63,
+	TIPO_I_REST0 =0x3,
+	TIPO_I_JALR =0x43,
+	TIPO_JAL =0x6F,
+	TIPO_JALR = 0x67
+	 	
 };
-
-
-enum INSTRUCTIONS {
-	i_ADD	= 0x2,
-	i_SUB	= 0x3,
-	i_ADDi	= 0x8,
-	i_SHIFT	= 0x9,
-	i_AND	= 0x4,
-	i_OR	= 0x5,
-	i_NOT	= 0xA,
-	i_XOR	= 0x6,
-	i_SLT	= 0x7,
-	i_LW	= 0,
-	i_SW	= 0x1,
-	i_LUI	= 0xB,
-	i_BEQ	= 0xC,
-	i_BNE	= 0xD,
-	i_J		= 0xE,
-	i_JAL	= 0xF
+// Opcodes referentes ao tipo J - notar que o opcode já resolve
+enum Opcode_U{
+u_LUI = 0x37,
+u_AUIPC=0X17
 };
-
+// func3 para definir tipoB
+enum funct3_tipoB{
+f3_BEQ = 0x0,
+f3_BNE = 0x1,
+f3_BLT = 0x4,
+f3_BGE = 0x5,
+f3_BLTU = 0x6,
+f3_BGEU = 0x7
+};
+// definir tipoI,ADDI ...
+enum funct3_tipoI_2{
+f3_ADDI = 0x0,
+f3_SLTI = 0x2,
+f3_SLTIU = 0x3,
+f3_XORI = 0x4,
+f3_ORI = 0x6,
+f3_ANDI = 0x7
+};
+// definir tipoI, lw
+enum funct3_tipoI_1{
+f3_LB = 0x0,
+f3_LH = 0x1,
+f3_LW = 0x2,
+f3_LBU = 0x4,
+f3_LHU = 0x5
+};
+// funções stores
+enum funct3_tipoS{
+f3_SB = 0x0,
+f3_SH = 0x1,
+f3_SW = 0x2
+};
+enum funct7_tipoR{
+f7_RESTO = 0x00,
+f7_SRA_SUB = 0x20
+};
+enum funct7_tipoI_shamt{
+f7_RESTO_I = 0x00,
+f7_SRAI = 0x20
+};
+enum funct7_tipoI_shamt{
+f3_SLLI = 0x1,
+f3_SRLI_SRAI = 0x5
+};
+enum funct3_tipoR{
+f3_ADD_SUB = 0x0,
+f3_SLL = 0x1,
+f3_SLT = 0x2,
+f3_SLTU = 0x3,
+f3_XOR = 0x4,
+f3_SRL_SRA = 0x5,
+f3_OR = 0x6,
+f3_AND = 0x7
+};
 
 #endif /* SHARED_H_ */
